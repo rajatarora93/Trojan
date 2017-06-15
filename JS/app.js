4bdbc3cfcc4c154e395864c29676de19d9aa8bcc
Rajat <rj.arora93@gmail.com>
2017-06-15 22:43:39 +0530

    Updated app.js file

--- JS/app.js
+++ JS/app.js
@@ -21,12 +21,42 @@
                      templateUrl: 'Templates/Login.html',
                      controller: 'loginController'
                      }).
+                     when("/ApplicationHome", {
+                     templateUrl: 'Templates/ApplicationHome.html',
+                     controller: 'appHomeController'
+                     }).
                      when("/SignUp", {
                      templateUrl: 'Templates/SignUp.html',
                      controller: 'signupController'
                      }).
+                     when("/FoodMenu", {
+                     templateUrl: 'Templates/FoodMenu.html',
+                     controller: 'signupController'
+                         }).
                      otherwise({
-                         redirectTo: '/'
+                         redirectTo: ''
                      });
                      $locationProvider.html5Mode(true);//code embedded to remove # from href link and url.
                  });
+app.run(function ($rootScope, $route, $location) {
+    $rootScope.$on('$locationChangeSuccess', function  () {
+        $rootScope.actualLocation = $location.path();
+        console.log($location.path());
+        console.log("rootscope"+$rootScope.username);
+    });
+    $rootScope.$watch(function () { return $location.path() }, function (newLocation, oldLocation) {
+        if ($rootScope.username == '') {
+            if ($location.path() == '/login') {
+                console.log($location.path());
+                $location.path("/login");
+            }
+            else if ($location.path() == '/SignUp') {
+                $location.path("/SignUp");
+            }
+            else if ($location.path() == '/ApplicationHome') {
+                console.log('ApplicationHomeSuccess');
+                $location.path('/ApplicationHome');
+            }
+        }
+    });
+});
