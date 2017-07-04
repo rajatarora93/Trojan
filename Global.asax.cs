4ae9a33860863bd07947f05399d0aca018322a23
rajatarora93 <rj.arora93@gmail.com>
2017-07-04 04:25:21 +0100

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace AngularJSWebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_BeginRequest()
+        {
+            string[] allowedOrigin = new string[] {"http://localhost:6722"};
+            var origin = HttpContext.Current.Request.Headers["Origin"];
+            if (origin != null && allowedOrigin.Contains(origin))
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", origin);
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "*");
+            }
+        }
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
---
+++ UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using AngularJSWebAPI.Models;
+
+namespace AngularJSWebAPI.Controllers
+{
+   
+    public class UserController : ApiController
+    {
+        User[] users = new User[]
+        {
+            new User() {Username="rajat.arora93@gmail.com",Password="hardworke" },
+            new User() {Username="rj.arora93@gmail.com",Password="hardworke" }
+        };
+
+        [HttpGet]
+        public JsonResult<Models.User>GetUser(string name)
+        {
+            try
+            {
+                Console.WriteLine("Web API");
+                var user = users.FirstOrDefault(p => p.Username == name);
+                if(user!=null)
+                {
+                    return Json<Models.User>(user);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+
+        }
+    }
+}
