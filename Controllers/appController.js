06787ee5d3ac39fedd7d51ff87b3200cf8d5e999
rajatarora93 <rj.arora93@gmail.com>
2017-07-05 08:46:24 +0530

    Update appController.js

--- Controllers/appController.js
+++ Controllers/appController.js
@@ -1,4 +1,4 @@
-﻿app.controller('appController', ['$scope','$rootScope','$location', function ($scope,$rootScope,$location) {
+app.controller('appController', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
     $scope.logout = function () {
         bootbox.confirm("Are you sure you want to logout", function (result) {
             if (result) {
@@ -7,35 +7,44 @@
                 $scope.$apply(function () {
                     $location.path("/ApplicationHome");
                 })
-                console.log("logoutController"+$location.path());
+                console.log("logoutController" + $location.path());
             }
         });
     }
 }
 ]);
 
-app.controller('loginController', ['$scope', '$rootScope','$location', function ($scope, $rootScope, $location) {
-    $scope.validate = function (form) {
-        if ($scope.user.username != $scope.user.password) {
-            $scope.message = "Oops! Credentials given were wrong! Please Re-try with valid credentials..!";
-            angular.element("#login")[0].reset();
-        }
-        else {
-            $rootScope.username = $scope.user.username;
-            angular.element("#login")[0].reset();
-            $scope.message = '';
-            bootbox.alert("Successful Login");
-            $location.path("/FoodMenu");
-           
-        }
+app.controller('loginController', ['$scope', '$rootScope', '$location','myFactory', function ($scope, $rootScope, $location,myFactory) {
+    $scope.validate = function () {
+        myFactory.getUser($scope.user.username).then(function (data) {
+            if (data.Username == undefined || $scope.user.password != data.Password) {
+                $scope.message = "Oops! Credentials given were wrong! Please Re-try with valid credentials..!";
+                var master = { username: '', password: '' };
+                $scope.user = angular.copy(master);
+                $scope.loginform.$setPristine();
+            }
+            else {
+                $rootScope.username = $scope.user.username;
+                angular.element("#login")[0].reset();
+                $scope.message = '';
+                bootbox.alert("Successful Login");
+                $location.path("/FoodMenu");
+            }
+        });
+       // myFactory.getPosts();
+    }
+    $scope.hideMessage=function()
+    {
+        $scope.message = '';
     }
 }
 ]);
 
 app.controller('signupController', ['$scope', function ($scope) {
-    $scope.signup=function(form)
-    {
+    $scope.signup = function (form) {
         bootbox.alert("Registration details saved");
     }
 }
 ]);
+
+
