e94c45f0c0369112763674d327b4c6654ba896f9
rajatarora93 <rj.arora93@gmail.com>
2017-07-05 08:44:26 +0530

    Add files via upload

---
+++ JS/services.js
@@ -0,0 +1,22 @@
+﻿var services = angular.module('services', []);
+services.factory('myFactory', function ($resource, $http) {
+    return {
+        getUser: function (name) {
+            console.log("myFactory");
+            return $http.get("http://localhost:35675/api/User/GetUser?name="+name).
+            then(function (response) {
+                var data = response.data;
+                return data;
+            })
+            //var urlLogin = "http://localhost:35675/api/User/GetUser/:name";
+            //return $resource(urlLogin, { name: '@name' }).get({ name: name });
+        },
+        getPosts: function () {
+            return $http.get("http://localhost:35675/api/User/Get")
+           .then(function (response) {
+               var data = response.data;
+               console.log(data);
+           })
+        }
+    };
+});
