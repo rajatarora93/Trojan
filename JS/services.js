2720ed4ebe1cec94b6d25c77442e32a031c0a499
rajatarora93 <rj.arora93@gmail.com>
2017-09-17 20:50:03 +0530

    Update services.js

--- JS/services.js
+++ JS/services.js
@@ -1,22 +1,58 @@
-﻿var services = angular.module('services', []);
+var services = angular.module("services", []);
+services.service('sharedData', function () {
+    this.item = '';
+    this.setItem = function (itemcode) {
+        this.item = itemcode;
+        console.log(this.item);
+    };
+    this.getItem = function () {
+        return this.item;
+    };
+});
+
+services.factory('CartServiceFactory', function () {
+    return{
+        cart: function () {
+            console.log("Cart");
+            return new shoppingCart('Cart');
+        }
+    };
+});
+
+services.factory('OrderServiceFactory', function () {
+    return {
+        bag: function () {
+            console.log("bag");
+             return new customerOrders('Customer');
+        }
+    };
+});
+
 services.factory('myFactory', function ($resource, $http) {
     return {
         getUser: function (name) {
             console.log("myFactory");
-            return $http.get("http://localhost:35675/api/User/GetUser?name="+name).
-            then(function (response) {
-                var data = response.data;
-                return data;
-            })
+            return $http.get("http://localhost:58850/api/User/GetUser?name=" + name).
+                then(function (response) {
+                    var data = response.data;
+                    return data;
+                })
             //var urlLogin = "http://localhost:35675/api/User/GetUser/:name";
             //return $resource(urlLogin, { name: '@name' }).get({ name: name });
         },
-        getPosts: function () {
-            return $http.get("http://localhost:35675/api/User/Get")
-           .then(function (response) {
-               var data = response.data;
-               console.log(data);
-           })
-        }
+        getFoodItems: function () {
+            return $http.get("http://localhost:58850/api/FoodItems/GetFoodItems")
+                .then(function (response) {
+                    var data = response.data;
+                    return data;
+                })
+        },
+         getFoodDetails: function () {
+            return $http.get("http://localhost:58850/api/FoodItems/GetFoodDetails")
+                .then(function (response) {
+                    var data = response.data;
+                    return data;
+                })
+         }
     };
 });
